13d564760ab20f9063211470ec23338edc91472d
Andrew Henning-Kolberg <andrew.kolberg@gmail.com>
2020-06-14 00:03:35 +0000

    Change landing page to series index, update views
    This commit makes the root landing page the Series index instead of the
    welcomes pages. Views are also updated for the series show page and
    episode show page to properly link to each other.

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -2,4 +2,8 @@ class Episode < ApplicationRecord
   has_many :episode_hexes
   has_many :hexes, through: :episode_hexes
   belongs_to :series
+
+  delegate :name,
+    to: :series,
+    prefix: true
 end

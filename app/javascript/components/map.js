9ebab1f09690ed2d5793015c5cbf2437fa23594a
Andrew Henning-Kolberg <andrew.kolberg@gmail.com>
2020-07-11 15:51:48 +0000

    Draw map image on on canvas on show page
    This commit updates the map show page to draw the image on a canvas.

---
+++ app/javascript/components/map.js
@@ -0,0 +1,37 @@
+class GameMap {
+  constructor(selector) {
+    this.element = document.getElementById(selector)
+
+    var images = require.context('../images', true)
+    var imagePath = (name) => images(name, true)
+
+    this.mapImageUrl = imagePath('./maps/' + this.element.dataset.mapBackground)
+  }
+
+  insertHexGrid() {
+    this.element.innerHTML = this._html()
+
+    var ctx = document.getElementById('map-component__canvas').getContext('2d')
+
+    var image = new Image(100, 100)
+
+    image.onload = function() {
+      ctx.drawImage(image, 0, 0)
+    }
+    image.src = this.mapImageUrl
+    image.id = "map-image"
+  }
+
+  /******
+   * private
+   */
+
+  _html() {
+    return `
+      <canvas id="map-component__canvas">
+      </canvas>
+    `
+  }
+}
+
+export { GameMap }
---
+++ app/javascript/views/maps/show.js
@@ -0,0 +1,6 @@
+import { GameMap } from '../../components/map.js'
+
+$(document).on('turbolinks:load', function() {
+  var gameMap = new GameMap('map')
+  gameMap.insertHexGrid()
+})

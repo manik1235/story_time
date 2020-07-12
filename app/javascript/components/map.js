6a0d9c9b589563cfc8bc1b103a180aed3b744e61
Andrew Henning-Kolberg <andrew.kolberg@gmail.com>
2020-07-12 15:41:56 +0000

    Add number of shifts to Axis, DRY up lines. For q axis
    This commit adds the `shifts` property to the `Axis` class. This is used
    to shift the lines the appropriate number of times.
    
    The `shiftx0y0` method is moved in to Axis as `shift`
    
    All these changes are for q axis only.

--- app/javascript/components/map.js
+++ app/javascript/components/map.js
@@ -175,40 +175,21 @@ class GameMap {
       0,
       undefined,
       94,
-      true
+      true,
+      2
     )
 
     return { q }
   }
 
   _drawHexAxisLines(ctx, axis) {
-    var shifted
-
-    // Draw first set of lines with first set of offsets
-    axis.drawDashedLines(ctx, axis)
-
-    /*
-    // Shift first set of offsets to second set of offsets
-    shifted = axis.shiftx0y0()
-    axis.x0 = shifted.x0
-    axis.y0 = shifted.y0
-
-    // Draw second set of lines with second set of offsets
-    this._drawDashedLines(ctx, axis.dash, axis.x0, axis.y0, axis.m, axis.mapWidth, axis.mapHeight, axis.hexDiameter, axis.color, axis.xBuffer)
-
-    // Shift second set of offsets to third set of offsets
-    shifted = axis.shiftx0y0()
-    axis.x0 = shifted.x0
-    axis.y0 = shifted.y0
-
-    // Draw third set of lines with third set of offsets
-    this._drawDashedLines(ctx, axis.dash, axis.x0, axis.y0, axis.m, axis.mapWidth, axis.mapHeight, axis.hexDiameter, axis.color, axis.xBuffer)
-    */
+    // Draw all sets of lines for one axis
+    axis.drawHexDashedLines(ctx)
   }
 }
 
 class Axis {
-  constructor(name, degrees, xOffset, hexDiameter, mapWidth, mapHeight, dashFilledLength, dashBlankLength, color, xBuffer, x0, y0, calcx0) {
+  constructor(name, degrees, xOffset, hexDiameter, mapWidth, mapHeight, dashFilledLength, dashBlankLength, color, xBuffer, x0, y0, calcx0, shifts) {
     this.name = name
     this.degrees = degrees
     this.xOffset = xOffset
@@ -225,6 +206,7 @@ class Axis {
       this.x0 = x0
     }
     this.y0 = y0
+    this.shifts = shifts
   }
 
   get radians() {
@@ -259,27 +241,43 @@ class Axis {
     return degrees * (Math.PI / 180)
   }
 
-  shiftx0y0() {
-    var x0
-    var y0
-
+  shift(x0, y0) {
     if (this.radians === 0 || this.radians === Math.PI) {
       // Shift horizontal lines
-      x0 = this.x0 + this.dashFilledLength * 1.5
-      y0 = this.y0 + this.hexDiameter / 2
+      x0 = x0 + this.dashFilledLength * 1.5
+      y0 = y0 + this.hexDiameter / 2
     } else if (this.radians === Math.PI / 2 || this.radians === 3 * Math.PI / 2) {
       // Shift vertical lines
       console.log("Not implemented")
     } else {
       // Shift angled lines
-      x0 = this.x0 + Math.cos(this.radians) * this.dashFilledLength * 3
-      y0 = this.y0 + Math.sin(this.radians) * this.dashFilledLength
+      x0 = x0 + Math.cos(this.radians) * this.dashFilledLength * 3
+      y0 = y0 + Math.sin(this.radians) * this.dashFilledLength
     }
 
     return { x0, y0 }
   }
 
-  drawDashedLines(ctx) {
+  drawHexDashedLines(ctx) {
+    var shifted
+    var x0 = this.x0
+    var y0 = this.y0
+
+    // Draw first set of lines
+    this._drawDashedLines(ctx, x0, y0)
+
+    for (var i = this.shifts; i > 0; i--) {
+      // Shift set of offsets to next set of offsets
+      shifted = this.shift(x0, y0)
+      x0 = shifted.x0
+      y0 = shifted.y0
+
+      // Draw shifted lines
+      this._drawDashedLines(ctx, x0, y0)
+    }
+  }
+
+  _drawDashedLines(ctx, x0, y0) {
     var line
     var xStep = this.xStep
     var yStep = this.yStep
@@ -287,15 +285,15 @@ class Axis {
     ctx.strokeStyle = this.color
     ctx.setLineDash(this.dash)
     ctx.beginPath()
-    for (var xi = this.x0, yi = this.y0; xi <= this.mapWidth + this.xBuffer && yi <= this.mapHeight; xi += this.xStep, yi += this.yStep) {
-      line = this._getLinex1x2y1y2(xi, yi)
+    for (var xi = x0, yi = y0; xi <= this.mapWidth + this.xBuffer && yi <= this.mapHeight; xi += this.xStep, yi += this.yStep) {
+      line = this._getLineCoordinates(xi, yi)
       ctx.moveTo(line.x1, line.y1);
       ctx.lineTo(line.x2, line.y2);
       ctx.stroke();
     }
   }
 
-  _getLinex1x2y1y2(x1, y1) {
+  _getLineCoordinates(x1, y1) {
     var x2
     var y2
     var b = y1 - this.m * x1

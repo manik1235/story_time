0072307b3a9dc140e2043905c0b029308d396aa7
Andrew Henning-Kolberg <andrew.kolberg@gmail.com>
2020-07-12 19:03:03 +0000

    Add maps helper, to_json method on Axis
    This commit updates the show page to include the data required for the
    hex line in the html element attributes.
    
    It also adds a helper method to the Axis model that will return the
    appropriate data items in JSON format for the attribute.

--- app/helpers/maps_helper.rb
+++ app/helpers/maps_helper.rb
@@ -1,2 +1,12 @@
 module MapsHelper
+  def data_attributes(map)
+    data = ""
+
+    data += "data-map-background=#{map.background}"
+    map.axes.each do |axis|
+      data += " data-#{axis.name}=#{axis.to_json}"
+    end
+
+    data
+  end
 end
--- app/models/axis.rb
+++ app/models/axis.rb
@@ -1,3 +1,12 @@
 class Axis < ApplicationRecord
   belongs_to :map
+
+  def to_json
+    columns = %i(degrees x_offset y_offset hex_diameter map_width map_height dash_filled_length dash_blank_length color)
+
+    columns.reduce({}) do |a, column|
+      a[column] = public_send(column)
+      a
+    end.to_json
+  end
 end

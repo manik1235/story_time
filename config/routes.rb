e10c66313dc341889c6739226b4082cf8fb29f72
Andrew Henning-Kolberg <akolberg@squaremouth.com>
2020-07-21 20:08:27 -0400

    Remove welcomes scaffold
    This commit removes the welcomes scaffold and seed data, since it is not
    used anymore.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   root "series#index"
 
-  resources :axis, only: [:update, :edit]
+  resources :axis, only: :update
   resources :episodes
   resources :hexes
   resources :maps
   resources :series
-  resources :welcomes
 
   if Rails.env.development? || Rails.env.test?
     get 'jasmine', to: 'jasmine#index'

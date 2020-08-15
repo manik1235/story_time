77e829f7a2b496bfee4c3614801156ff18046c7a
Andrew Henning-Kolberg <akolberg@squaremouth.com>
2020-08-15 12:35:46 -0400

    Restructure private and public axis interface
    This commit restructures the public and private interface for the Axis
    component. All private methods are prefixed with `_`.

--- app/javascript/components/axis.js
+++ app/javascript/components/axis.js
@@ -4,9 +4,11 @@ class Axis {
     this._axisContext = ctx
 
     this._updateAxis()
-    this.drawLines(this._axisContext)
+    this._drawLines(this._axisContext)
     this._addUpdateListener()
   }
+
+  // Private
 
   _addUpdateListener() {
     let elements = document.getElementsByClassName(this._selector)
@@ -17,8 +19,115 @@ class Axis {
       element.addEventListener('change', function(event) {
         // TODO: Use the event to selectively update the proper property
         that._updateAxis()
-        that.drawLines(that._axisContext)
+        that._drawLines(that._axisContext)
       })
+    }
+  }
+
+  get _dash() {
+    return [this.dashFilledLength, this.dashBlankLength]
+  }
+
+  _drawLines(ctx) {
+    var shifted
+    var x0 = this._x0
+    var y0 = this._y0
+
+    // Clear the old lines from the canvas
+    ctx.clearRect(0, 0, this.mapWidth, this.mapHeight)
+
+    // Draw first set of lines
+    this._drawDashedLines(ctx, x0, y0)
+
+    for (var i = this._shifts; i > 0; i--) {
+      // Shift set of offsets to next set of offsets
+      shifted = this._shift(x0, y0)
+      x0 = shifted.x0
+      y0 = shifted.y0
+
+      // Draw shifted lines
+      this._drawDashedLines(ctx, x0, y0)
+    }
+  }
+
+  _drawDashedLines(ctx, x0, y0) {
+    var line
+
+    ctx.strokeStyle = this.color
+    ctx.setLineDash(this._dash)
+    ctx.beginPath()
+    for (var xi = x0, yi = y0; xi <= this.mapWidth + this._xBuffer && yi <= this.mapHeight; xi += this._xStep, yi += this._yStep) {
+      line = this._getLineCoordinates(xi, yi)
+      ctx.moveTo(line.x1, line.y1)
+      ctx.lineTo(line.x2, line.y2)
+      ctx.stroke()
+    }
+  }
+
+  _getLineCoordinates(x1, y1) {
+    var x2
+    var y2
+    var b = y1 - this._m * x1
+
+    var possible_y = this._m * this.mapWidth + b
+    var possible_x = (this.mapHeight - b) / this._m
+
+    if (this._m >= 0) {
+      if (possible_y > this.mapHeight) {
+        x2 = possible_x
+        y2 = this.mapHeight
+      } else {
+        x2 = this.mapWidth
+        y2 = possible_y
+      }
+    } else {
+      if (possible_x < 0) {
+        x2 = 0
+        y2 = b
+      } else {
+        x2 = possible_x
+        y2 = this.mapHeight
+      }
+    }
+
+    return { x1, x2, y1, y2 }
+  }
+
+  get _m() {
+    return Math.tan(this._radians)
+  }
+
+  get _radians() {
+    return this.degrees * (Math.PI / 180)
+  }
+
+  _shift(x0, y0) {
+    if (this._radians === 0 || this._radians === Math.PI) {
+      // Shift horizontal lines
+      x0 = x0 + this.dashFilledLength * 1.5
+      y0 = y0 + this.hexDiameter / 2
+    } else if (this._radians === Math.PI / 2 || this._radians === 3 * Math.PI / 2) {
+      // Shift vertical lines
+      console.log("Not implemented")
+    } else {
+      // Shift angled lines
+      x0 = x0 + Math.cos(this._radians) * this.dashFilledLength * 3
+      y0 = y0 + Math.sin(this._radians) * this.dashFilledLength
+    }
+
+    return { x0, y0 }
+  }
+
+  get _shifts() {
+    if (this._radians === 0 || this._radians === Math.PI) {
+      // Horizontal
+      return 1
+    } else if (this._radians === Math.PI / 2 || this._radians === 3 * Math.PI / 2) {
+      // Vertical
+      console.log("Not implemented")
+    } else {
+      // Angled
+      return 2
     }
   }
 
@@ -45,148 +154,11 @@ class Axis {
     this.color = axisObject.color
   }
 
-  get dash() {
-    return [this.dashFilledLength, this.dashBlankLength]
-  }
-
-  drawLines(ctx) {
-    var shifted
-    var x0 = this._x0
-    var y0 = this._y0
-
-    // Clear the old lines from the canvas
-    ctx.clearRect(0, 0, this.mapWidth, this.mapHeight)
-
-    // Draw first set of lines
-    this._drawDashedLines(ctx, x0, y0)
-
-    for (var i = this.shifts; i > 0; i--) {
-      // Shift set of offsets to next set of offsets
-      shifted = this.shift(x0, y0)
-      x0 = shifted.x0
-      y0 = shifted.y0
-
-      // Draw shifted lines
-      this._drawDashedLines(ctx, x0, y0)
-    }
-  }
-
-  get m() {
-    return Math.tan(this.radians)
-  }
-
-  shift(x0, y0) {
-    if (this.radians === 0 || this.radians === Math.PI) {
-      // Shift horizontal lines
-      x0 = x0 + this.dashFilledLength * 1.5
-      y0 = y0 + this.hexDiameter / 2
-    } else if (this.radians === Math.PI / 2 || this.radians === 3 * Math.PI / 2) {
-      // Shift vertical lines
-      console.log("Not implemented")
-    } else {
-      // Shift angled lines
-      x0 = x0 + Math.cos(this.radians) * this.dashFilledLength * 3
-      y0 = y0 + Math.sin(this.radians) * this.dashFilledLength
-    }
-
-    return { x0, y0 }
-  }
-
-  get shifts() {
-    if (this.radians === 0 || this.radians === Math.PI) {
-      // Horizontal
-      return 1
-    } else if (this.radians === Math.PI / 2 || this.radians === 3 * Math.PI / 2) {
-      // Vertical
-      console.log("Not implemented")
-    } else {
-      // Angled
-      return 2
-    }
-  }
-
-  get radians() {
-    return this._radians(this.degrees)
-  }
-
-  get xStep() {
-    if (this.radians === 0 || this.radians === Math.PI) {
-      // Horizontal
-      return 0
-    } else if (this.radians === Math.PI / 2 || this.radians === 3 * Math.PI / 2) {
-      // Vertical
-      console.log("Not implemented")
-    } else {
-      // Angled
-      return this.hexDiameter
-    }
-  }
-
-  get yStep() {
-    if (this.radians === 0 || this.radians === Math.PI) {
-      // Horizontal
-      return this.hexDiameter
-    } else if (this.radians === Math.PI / 2 || this.radians === 3 * Math.PI / 2) {
-      // Vertical
-      console.log("Not implemented")
-    } else {
-      // Angled
-      return 0
-    }
-  }
-
-  _drawDashedLines(ctx, x0, y0) {
-    var line
-
-    ctx.strokeStyle = this.color
-    ctx.setLineDash(this.dash)
-    ctx.beginPath()
-    for (var xi = x0, yi = y0; xi <= this.mapWidth + this._xBuffer && yi <= this.mapHeight; xi += this.xStep, yi += this.yStep) {
-      line = this._getLineCoordinates(xi, yi)
-      ctx.moveTo(line.x1, line.y1)
-      ctx.lineTo(line.x2, line.y2)
-      ctx.stroke()
-    }
-  }
-
-  _getLineCoordinates(x1, y1) {
-    var x2
-    var y2
-    var b = y1 - this.m * x1
-
-    var possible_y = this.m * this.mapWidth + b
-    var possible_x = (this.mapHeight - b) / this.m
-
-    if (this.m >= 0) {
-      if (possible_y > this.mapHeight) {
-        x2 = possible_x
-        y2 = this.mapHeight
-      } else {
-        x2 = this.mapWidth
-        y2 = possible_y
-      }
-    } else {
-      if (possible_x < 0) {
-        x2 = 0
-        y2 = b
-      } else {
-        x2 = possible_x
-        y2 = this.mapHeight
-      }
-    }
-
-    return { x1, x2, y1, y2 }
-  }
-
-  _radians(degrees) {
-    return degrees * (Math.PI / 180)
-  }
-
   get _x0() {
-    if (this.m > 0) {
+    if (this._m > 0) {
       // Calculate the extra distance needed to start to the left of the canvas
       //   if the slope is positive
-      return -this.mapHeight / this.m + this._xOffset
+      return -this.mapHeight / this._m + this._xOffset
     } else {
       // Otherwise only the xOffset is needed
       return this._xOffset
@@ -194,13 +166,39 @@ class Axis {
   }
 
   get _xBuffer() {
-    if (this.m < 0) {
+    if (this._m < 0) {
       // Calculate the extra buffer needed when drawing lines that will pass the
       //   mapWidth boundary
-      return -this.m * this.mapHeight + this._xOffset
+      return -this._m * this.mapHeight + this._xOffset
     } else {
       // No extra buffer needed because the lines will always start on or to the
       //   left of the canvas.
+      return 0
+    }
+  }
+
+  get _xStep() {
+    if (this._radians === 0 || this._radians === Math.PI) {
+      // Horizontal
+      return 0
+    } else if (this._radians === Math.PI / 2 || this._radians === 3 * Math.PI / 2) {
+      // Vertical
+      console.log("Not implemented")
+    } else {
+      // Angled
+      return this.hexDiameter
+    }
+  }
+
+  get _yStep() {
+    if (this._radians === 0 || this._radians === Math.PI) {
+      // Horizontal
+      return this.hexDiameter
+    } else if (this._radians === Math.PI / 2 || this._radians === 3 * Math.PI / 2) {
+      // Vertical
+      console.log("Not implemented")
+    } else {
+      // Angled
       return 0
     }
   }

b5a049150aecf67ad42aee92a4558e70c034a2b4
Andrew Henning-Kolberg <akolberg@squaremouth.com>
2020-10-10 11:53:05 -0400

    Track x and y mouse position on move
    This commit tracks the mouse movement over the canvases and displays the
    values above the map

--- app/javascript/views/maps/show.js
+++ app/javascript/views/maps/show.js
@@ -1,7 +1,21 @@
 import { GameMap } from '../../components/game_map.js'
 
 $(document).on('turbolinks:load', function() {
+  setupGameMap()
+  bindEvents()
+})
+
+function setupGameMap() {
   let selector = 'map'
   let gameMap = new GameMap(selector)
   gameMap.addMap()
-})
+}
+
+function bindEvents() {
+  $(".js-canvas").last().bind("mousemove", function(event) {
+    var x = event.offsetX,
+      y = event.offsetY
+
+    $(".js-debug").text(`${x},${y}`)
+  })
+}
